fa4852d048404fd176dbbe44ccbaf087d8ad9e63
Elvis Montaluisa <elvis.montaluisa@epn.edu.ec>
2022-03-11 23:56:28 -0500

    Examen 2

---
+++ Examen2/arkanoid/src/app/components/arkanoid/arkanoid.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArkanoidComponent } from './arkanoid.component';
+
+describe('ArkanoidComponent', () => {
+  let component: ArkanoidComponent;
+  let fixture: ComponentFixture<ArkanoidComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArkanoidComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArkanoidComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Examen2/arkanoid/src/app/components/arkanoid/arkanoid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-arkanoid',
+  templateUrl: './arkanoid.component.html',
+  styleUrls: ['./arkanoid.component.css']
+})
+export class ArkanoidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
